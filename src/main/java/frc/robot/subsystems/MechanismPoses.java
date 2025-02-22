94916f4968df646eb4b1da0d80d98f728d31e1f6
Brennan Bibic <brennan.bibic@gmail.com>
2025-02-22 11:35:04 -0500

    advantage scope poses kinda work

--- src/main/java/frc/robot/subsystems/MechanismPoses.java
+++ src/main/java/frc/robot/subsystems/MechanismPoses.java
@@ -7,6 +7,8 @@ import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.networktables.DoubleArrayPublisher;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.StructArrayPublisher;
+import edu.wpi.first.networktables.StructPublisher;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotContainer;
 import frc.robot.data.Constants.ElevatorConstants;
@@ -14,21 +16,22 @@ import frc.robot.data.Constants.PhysicalConstants;
 
 public class MechanismPoses extends SubsystemBase {
     private final NetworkTableInstance inst = NetworkTableInstance.getDefault();
-    private final NetworkTable mechanismTable = inst.getTable("Mechanism");
+    private final NetworkTable mechanismTable = inst.getTable("Mechanism3d");
 
     // Publishers for each mechanism's pose
-    private final DoubleArrayPublisher elevatorFirstStagePosePub = mechanismTable.getDoubleArrayTopic("elevatorFirstStagePose").publish();
-    private final DoubleArrayPublisher elevatorCarriagePosePub = mechanismTable.getDoubleArrayTopic("elevatorCarriagePose").publish();
-    private final DoubleArrayPublisher pivotPosePub = mechanismTable.getDoubleArrayTopic("pivotPose").publish();
+    private final StructPublisher<Pose3d> elevatorFirstStagePosePub = mechanismTable.getStructTopic("elevatorFirstStage", Pose3d.struct).publish();
+    private final StructPublisher<Pose3d> elevatorCarriagePosePub = mechanismTable.getStructTopic("elevatorCarriage", Pose3d.struct).publish();
+    private final StructPublisher<Pose3d> pivotPosePub = mechanismTable.getStructTopic("pivot", Pose3d.struct).publish();
+    private final StructPublisher<Pose3d> zeroCalibPosePub = mechanismTable.getStructTopic("zerocalib", Pose3d.struct).publish();
 
     // Base transforms for each mechanism (relative to robot center)
     private static final Transform3d ELEVATOR_BASE = new Transform3d(
-        new Translation3d(PhysicalConstants.withBumperBotHalfWidth - 0.1, 0.0, 0.1), // Elevator is near the front of the robot
+        new Translation3d(0.1208, 0, 0.0699), // Elevator is near the front of the robot
         new Rotation3d(0, 0, 0)
     );
 
     private static final Transform3d PIVOT_BASE = new Transform3d(
-        new Translation3d(PhysicalConstants.withBumperBotHalfWidth - 0.1, 0.0, 0.1), // Pivot is at the same base as elevator
+        new Translation3d(0.2858, 0, 0.4128), // Pivot is at the same base as elevator
         new Rotation3d(0, 0, 0)
     );
 
@@ -36,7 +39,8 @@ public class MechanismPoses extends SubsystemBase {
     private static final double FIRST_STAGE_START_HEIGHT = ElevatorConstants.MAX_ELEVATOR_HEIGHT / 2.0; // Height where first stage starts moving
 
     public MechanismPoses() {
-        // Constructor
+        // Publish zeroed calibration pose
+        zeroCalibPosePub.set(new Pose3d());
     }
 
     @Override
@@ -54,13 +58,13 @@ public class MechanismPoses extends SubsystemBase {
 
         if (elevatorHeight <= FIRST_STAGE_START_HEIGHT) {
             // Only carriage moves in first half
-            carriageHeight = elevatorHeight;
+            carriageHeight = elevatorHeight + 0.0254;
             firstStageHeight = 0;
         } else {
             // First stage starts moving in second half
             double remainingHeight = elevatorHeight - FIRST_STAGE_START_HEIGHT;
             firstStageHeight = remainingHeight;
-            carriageHeight = FIRST_STAGE_START_HEIGHT + remainingHeight;
+            carriageHeight = FIRST_STAGE_START_HEIGHT + remainingHeight + 0.0254;
         }
 
         // Create first stage pose
@@ -75,8 +79,9 @@ public class MechanismPoses extends SubsystemBase {
             ELEVATOR_BASE.getRotation()
         );
 
-        publishPose(elevatorFirstStagePosePub, firstStagePose);
-        publishPose(elevatorCarriagePosePub, carriagePose);
+        // Publish poses directly
+        elevatorFirstStagePosePub.set(firstStagePose);
+        elevatorCarriagePosePub.set(carriagePose);
     }
 
     private void updatePivotPose() {
@@ -89,17 +94,7 @@ public class MechanismPoses extends SubsystemBase {
             new Rotation3d(0, pivotAngle, 0).plus(PIVOT_BASE.getRotation())
         );
 
-        publishPose(pivotPosePub, pivotPose);
-    }
-
-    private void publishPose(DoubleArrayPublisher publisher, Pose3d pose) {
-        publisher.set(new double[] {
-            pose.getX(),
-            pose.getY(),
-            pose.getZ(),
-            pose.getRotation().getX(),
-            pose.getRotation().getY(),
-            pose.getRotation().getZ()
-        });
+        // Publish pose directly
+        pivotPosePub.set(pivotPose);
     }
 } 
